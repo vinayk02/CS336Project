c0acd3ea43c40d14a78c24cb9b670d6f016a5dd4
vinayk02 <80611746+vinayk02@users.noreply.github.com>
2022-04-08 02:20:51 -0400

    Update ProjectSQL.sql

--- ProjectSQL.sql
+++ ProjectSQL.sql
@@ -5,4 +5,4 @@ USE `cs336p1`;
 DROP TABLE IF EXISTS `userLogin`;
 
 CREATE TABLE `userLogin`(`email` varchar(50) NOT NULL DEFAULT '', `password` varchar(50) NOT NULL DEFAULT '', `first` varchar(50) DEFAULT NULL,
-	`last` varchar(50) DEFAULT NULL, `userName` varchar(50) DEFAULT NULL, PRIMARY KEY(`email`, `password`))ENGINE=InnoDB DEFAULT CHARSET=latin1;emaillastPRIMARY
+	`last` varchar(50) DEFAULT NULL, `userName` varchar(50) DEFAULT NULL, PRIMARY KEY(`email`, `password`))ENGINE=InnoDB DEFAULT CHARSET=latin1;
