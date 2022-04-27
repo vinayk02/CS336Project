6775934e5b0826daf89c83e3ce2d8f3543ce3086
vinayk02 <80611746+vinayk02@users.noreply.github.com>
2022-04-27 14:46:58 -0400

    Add files via upload

--- ProjectSQL.sql
+++ ProjectSQL.sql
@@ -6,3 +6,5 @@ DROP TABLE IF EXISTS `userLogin`;
 
 CREATE TABLE `userLogin`(`email` varchar(50) NOT NULL DEFAULT '', `password` varchar(50) NOT NULL DEFAULT '', `first` varchar(50) DEFAULT NULL,
 	`last` varchar(50) DEFAULT NULL, `userName` varchar(50) DEFAULT NULL, PRIMARY KEY(`email`, `password`))ENGINE=InnoDB DEFAULT CHARSET=latin1;
+    DROP TABLE IF EXISTS `auction`;
+    CREATE TABLE `auction`(`secretPrice` int DEFAULT NULL, `price` int DEFAULT NULL, `category` varchar(50) DEFAULT NULL, `itemName` varchar(50) NOT NULL DEFAULT '', `email` varchar(50) NOT NULL DEFAULT '', PRIMARY KEY(`itemName`, `email`))ENGINE=InnoDB DEFAULT CHARSET=latin1; 
