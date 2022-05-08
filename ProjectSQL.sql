3de8182a0ff14b588447b1274821b619a0a7f72b
vinayk02 <80611746+vinayk02@users.noreply.github.com>
2022-05-07 22:26:05 -0400

    Add files via upload

--- ProjectSQL.sql
+++ ProjectSQL.sql
@@ -7,6 +7,8 @@ DROP TABLE IF EXISTS `auction`;
 DROP TABLE IF EXISTS `alerts`;
 DROP TABLE IF EXISTS `bid`;
 DROP TABLE IF EXISTS `winningBid`;
+DROP TABLE IF EXISTS `repLogin`;
+DROP TABLE IF EXISTS `questions`;
 
 CREATE TABLE `userLogin`(`email` varchar(50) NOT NULL DEFAULT '', `password` varchar(50) NOT NULL DEFAULT '', `first` varchar(50) DEFAULT NULL,
 	`last` varchar(50) DEFAULT NULL, `userName` varchar(50) DEFAULT NULL, PRIMARY KEY(`email`, `password`))ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -20,4 +22,9 @@ CREATE TABLE `alerts`(`type` varchar(50) NOT NULL DEFAULT '', `name` varchar(50)
 
 CREATE TABLE `winningBid`( `category` varchar(50) DEFAULT NULL, `auctionName` varchar(50) NOT NULL DEFAULT '', `price` int, `winnerEmail` varchar(50) NOT NULL DEFAULT '', `auctionEmail` varchar(50) NOT NULL DEFAULT '', PRIMARY KEY(`auctionEmail` , `auctionName`))ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+CREATE TABLE `repLogin`(`email` varchar(50) NOT NULL DEFAULT '', `password` varchar(50) NOT NULL DEFAULT '', `userName` varchar(50) DEFAULT NULL, PRIMARY KEY(`email`, `password`))ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `questions`(`id` int, `email` varchar(50) NOT NULL DEFAULT '', `question` varchar(50) NOT NULL DEFAULT '', `answer` varchar(50), PRIMARY KEY(`id`))ENGINE=InnoDB DEFAULT CHARSET=latin1; 
+
 CREATE TABLE `adminLogin`(`email` varchar(50) NOT NULL DEFAULT '', `password` varchar(50) NOT NULL DEFAULT '', PRIMARY KEY(`email`, `password`))ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
